5e38e867bac4f15b50449f7547b2dd939745f3f0
arnav2k4 <161723756+arnav2k4@users.noreply.github.com>
2025-06-16 17:46:46 +0530

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,11 +1,10 @@
-fileName = input("Enter file name (include extension): ")
 try:
-    fr = open(fileName, "r")
+    fr = open('sample.txt', "r")
     print("Reading file content:")
     i = 1
     for line in fr:
         print("Line {}: {}".format(i, line.strip()))
         i += 1
+    fr.close()
 except FileNotFoundError:
-    print("Error: The file '{}' was not found.".format(aa))
-fr.close()
+    print("Error: The file 'sample.txt' was not found.")
